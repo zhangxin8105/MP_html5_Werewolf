068e3c440f05039a433e63144e30e1423ce3a820
qikai521 <qkgmm_521@163.com>
2016-11-25 15:29:49 +0800

    添加登录页面和进入房间页面

--- web/MainView.js
+++ web/MainView.js
@@ -6,43 +6,35 @@ function creatRoom() {
 
 }
 //根据频道号进行链接
-function enterRoom(roomNum) {
-
+function enterRoom() {
+    var roomNum  = $("#roomNum").val();
+    var welcomeString  =  playInfos[($("#loginidtext").val()).toString()].playerName +",欢迎您进入"+roomNum.toString();
+    alert(welcomeString)
 }
 function login() {
     var userid = $("#loginidtext").val();
     var password  = $("#loginpasswordtext").val();
-    alert("登录账号:"+userid+"登录密码"+password);
-    console.log(111);
-    if (userid && password){
+    if (userid){
        // 进行登录操作
-        var players = single.getInstance();
+        if (playInfos[userid.toString()]){
+            alert(playInfos[userid.toString()].playerName+",欢迎您进入狼人世界，呜哈哈哈哈（小武笑）");
+            document.getElementById("loginView").style.display = 'none';
+            document.getElementById("enterRoom").style.display = 'inline';
+        }else{
+            alert("登录失败");
+        }
     }
 }
 
-// 存储所有玩家信息的array
-var single = (function(){
-    var unique;
-    var playersInfo;
-    function getInstance(){
-        if( unique === undefined ){
-            unique = new Construct();
-        }
-        return unique;
-    }
-    function Construct() {
-        playersInfo = new Array;
-        var names = new  Array("何宁","张鑫","田嘉禾","陈斐","撖美霞","高磊","冬梅","李敏","陈林","刘玉鹏","曾宪武","张广忍","预言凯","刘源");
-        for (var i =0 ;i < 14 ;i++){
-            var player = new player(names[i],10000+i);
-        }
-    }
-    return {
-        getInstance : getInstance
-    }
-})();
 
+
+var playernames = new  Array("何宁","张鑫","田嘉禾","陈斐","撖美霞","高磊","冬梅","李敏","陈林","刘玉鹏","曾宪武","张广忍","预言凯","刘源");
+var playInfos = new  Array();
+for (var i = 0 ;i < 14 ; i++){
+    var player = new Player(playernames[i],10000+i);
+    playInfos[(i+10000).toString()] = player;
+}
 function Player(name,userid) {
-    var playerName = name;
-    var playerId = userid;
+    this.playerName = name;
+    this.playerid = userid;
 }
