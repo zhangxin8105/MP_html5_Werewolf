ef5b232a0fe70e7da9d33569a90bf6d3fe20b57a
zhangxin <zhangxin8105@gmail.com>
2016-12-06 13:43:06 +0800

    修改音频公共类

--- voiceDemo/voiceCom.js
+++ voiceDemo/voiceCom.js
@@ -1,5 +1,5 @@
 /*  */
-function playVoice(divName, text) {
+function playVoice(divName, url) {
 
 	$(divName).empty();
 
@@ -7,9 +7,10 @@ function playVoice(divName, text) {
 	// .append(
 	// '<EMBED src="'
 	// +
-	// 'http://api.okvoice.com/tts?apiKey=2594280bed1522810d28a717f57c64db&expires=1479998540&format=MP3&speed=1&text=%E5%BC%A0%E9%91%AB%E6%B5%8B%E8%AF%95&voice=cnfemale&signature=6dd0a119de2b3e9a5e4335ca4101d220939a3fad'
+	// 'http://api.okvoice.com/tts?apiKey=2594280bed1522810d28a717f57c64db&expires=1479998540&format=MP3&speed=1&url=%E5%BC%A0%E9%91%AB%E6%B5%8B%E8%AF%95&voice=cnfemale&signature=6dd0a119de2b3e9a5e4335ca4101d220939a3fad'
 	// + '" autostart="true" loop="true" width="0" height="0"></EMBED>');
-	$(divName).append(
-			'<EMBED src="' + text
-					+ '" autostart="true" loop="true" width="0" height="0"/>');
+
+	var playerDiv = $(divName);
+	playerDiv.append('<EMBED src="' + url
+			+ '" autostart="true" loop="true" width="0" height="0"/>');
 }
