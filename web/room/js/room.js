0d02957c2b92ce124ac865cdacccac297f2b436a
zhangxin <zhangxin8105@gmail.com>
2016-12-08 15:27:05 +0800

    使用新的解析工具类

--- web/room/js/room.js
+++ web/room/js/room.js
@@ -2,31 +2,18 @@
 
 ImportHelper.add([ //
 "../js/cookies.js",//
+"../js/urlparam.js",//
 ]);
 
 function roomSettings() {
 	// output: Gold%20Coast
 
-	urlParam = function(name) {
-		var results = new RegExp('[\?&]' + name + '=([^&#]*)')
-				.exec(window.location.href);
-		if (results == null) {
-			return null;
-		} else {
-			return results[1] || 0;
-		}
-	}
-
-	console.log(decodeURIComponent(urlParam('roomNum')));
-	console.log(decodeURIComponent(urlParam('playerid')));
-	console.log(decodeURIComponent(urlParam('playername')));
+	console.log(UrlParamHelper.getUrlParamByName('roomNum'));
+	console.log(UrlParamHelper.getUrlParamByName('playerid'));
+	console.log(UrlParamHelper.getUrlParamByName('playername'));
 
 	// window.location = "settings/roomSettings.html";
 }
 
 function testroom() {
-	location.href = "room.html?"//
-			+ 'roomNum=' + 1 //
-			+ "&playerid=" + 10001 //
-			+ "&playername=" + "zhangxin";
 }
