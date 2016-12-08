46466a8d06f4d42097badc1789a7329d9613c5b0
zhangxin <zhangxin8105@gmail.com>
2016-12-08 10:57:49 +0800

    增加一个加载js的工具类

---
+++ web/js/import.js
@@ -0,0 +1,74 @@
+/* import工具类 */
+
+var ImportHelper = (function() {
+	function add(script_dependencies) {
+		for (var s = 0; s < script_dependencies.length; s++) {
+			var findList = find(script_dependencies[s]);
+			if (findList.length > 0) {
+				continue;
+			}
+
+			script_element = document.createElement("script");
+			script_element.src = script_dependencies[s];
+
+			document.head.appendChild(script_element);
+		}
+	}
+
+	function remove(script_dependencies) {
+		dump();
+
+		for (var s = 0; s < script_dependencies.length; s++) {
+			var removeList = find(script_dependencies[s]);
+			for (var r = 0; r < removeList.length; r++) {
+				console.log("remove " + r + ":"
+						+ removeList[r].getAttribute("src"));
+				document.head.removeChild(removeList[r]);
+			}
+		}
+
+		dump();
+	}
+
+	function dump() {
+		var scriptList = document.getElementsByTagName("script");
+		for (var k = 0; k < scriptList.length; k++) {
+			console.log("dump " + k + ":" + scriptList[k].getAttribute("src"));
+		}
+	}
+
+	function find(scriptSrc) {
+		var scriptList = document.getElementsByTagName("script");
+		var findList = [];
+
+		for (var i = 0; i < scriptList.length; i++) {
+			if (scriptList[i].getAttribute("src") == scriptSrc) {
+				findList[findList.length] = scriptList[i];
+			}
+		}
+
+		return findList;
+	}
+
+	return {
+		add : add,
+		remove : remove,
+	};
+}());
+
+function test() {
+	ImportHelper.add([ //
+	"../js/cookies.js", //
+	"../js/cookies2.js",//
+	]);
+	ImportHelper.add([ //
+	"../js/cookies.js", //
+	"../js/cookies2.js",//
+	]);
+
+	ImportHelper.remove([ //
+	"../js/cookies2.js",//
+	]);
+}
+
+// test();
