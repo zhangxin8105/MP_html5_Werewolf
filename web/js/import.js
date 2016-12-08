38f8e7c8da7712c41a03b008d30eaa889c189a54
zhangxin <zhangxin8105@gmail.com>
2016-12-08 13:59:42 +0800

    修改import.js细节

--- web/js/import.js
+++ web/js/import.js
@@ -1,31 +1,74 @@
 /* import工具类 */
 
 var ImportHelper = (function() {
-	function add(script_dependencies) {
+	var scriptMap = null;
+
+	function init() {
+		var scriptOrgList = document.getElementsByTagName("script");
+		if (scriptMap == null || scriptMap.length != scriptOrgList.length) {
+			scriptMap = {};
+
+			console.log("scriptOrgList " + ":" + scriptOrgList);
+			for (var i = 0; i < scriptOrgList.length; i++) {
+				scriptMap[scriptOrgList[i].getAttribute("src")] = scriptOrgList[i];
+			}
+			for ( var i in scriptMap) {
+				console.log("scriptMap " + i + ":" + scriptMap[i]);
+			}
+		}
+
+		return scriptOrgList[scriptOrgList.length - 1];
+	}
+
+	function add(script_dependencies, callback) {
+		var callbackList = {};
 		for (var s = 0; s < script_dependencies.length; s++) {
-			var findList = find(script_dependencies[s]);
-			if (findList.length > 0) {
+			callbackList[script_dependencies[s]] = callback[s];
+		}
+
+		var lastChild = init();
+		var addList = [];
+		for (var s = 0; s < script_dependencies.length; s++) {
+			var sr = script_dependencies[s];
+
+			if (scriptMap[sr] != null) {
 				continue;
 			}
+			addList[addList.length] = sr;
+		}
+		document.head.removeChild(lastChild);
 
-			script_element = document.createElement("script");
+		if (addList.length == 0 && onloadCb) {
+			onloadCb();
+		}
+		for (var s = 0; s < addList.length; s++) {
+			var script_element = document.createElement("script");
 			script_element.type = "text/javascript";
 			script_element.src = script_dependencies[s];
 
-			document.head.childNodes.splice(0, 0, script_element);
-			// document.head.appendChild(script_element);
+			// var childNodes = new [ document.head.childNodes ];
+			// childNodes.splice(0, 0, script_element);
+			document.head.appendChild(script_element);
+
+			script_element.onload = function() {
+				var cb = callbackList[this.getAttribute("src")];
+				if (cb) {
+					cb();
+				}
+			}
 		}
+		document.head.appendChild(lastChild);
 	}
 
 	function remove(script_dependencies) {
+		init();
 		dump();
 
 		for (var s = 0; s < script_dependencies.length; s++) {
-			var removeList = find(script_dependencies[s]);
-			for (var r = 0; r < removeList.length; r++) {
-				console.log("remove " + r + ":"
-						+ removeList[r].getAttribute("src"));
-				document.head.removeChild(removeList[r]);
+			var remove = scriptMap[script_dependencies[s]];
+			console.log("remove " + ":" + remove.getAttribute("src"));
+			if (remove != null) {
+				document.head.removeChild(remove);
 			}
 		}
 
@@ -37,19 +80,9 @@ var ImportHelper = (function() {
 		for (var k = 0; k < scriptList.length; k++) {
 			console.log("dump " + k + ":" + scriptList[k].getAttribute("src"));
 		}
-	}
-
-	function find(scriptSrc) {
-		var scriptList = document.getElementsByTagName("script");
-		var findList = [];
-
-		for (var i = 0; i < scriptList.length; i++) {
-			if (scriptList[i].getAttribute("src") == scriptSrc) {
-				findList[findList.length] = scriptList[i];
-			}
+		for ( var i in scriptMap) {
+			console.log("scriptMap " + i + ":" + scriptMap[i]);
 		}
-
-		return findList;
 	}
 
 	return {
