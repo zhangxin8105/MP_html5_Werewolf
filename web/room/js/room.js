2af5907c2412b381cb739600aa8c36e57bdf3901
zhangxin <zhangxin8105@gmail.com>
2016-12-09 16:54:23 +0800

    增加store.js来存储数据

--- web/room/js/room.js
+++ web/room/js/room.js
@@ -19,8 +19,33 @@ function roomSettings() {
 	console.log(CookieHelper.getCookieByName(Werewolf.CONST.KEY_USER_NAME));
 	console.log(CookieHelper.getCookieByName(Werewolf.CONST.KEY_PASSWORD));
 
+	store.set('username', 'marcus');
+	store.set('zhangxin', 'zhx');
+	console.log(store.get('username'));
+	store.remove('username');
+
+	store.clear();
+
+	store.set('user', {
+		name : 'marcus',
+		likes : 'javascript'
+	});
+
+	var user = store.get('user');
+	console.log(user.name + ' likes ' + user.likes);
+
+	// Get all stored values
+	store.getAll().user.name == 'marcus';
+
+	// Loop over all stored values
+	store.forEach(function(key, val) {
+		console.log(key, '==', val);
+	})
 	// window.location = "settings/roomSettings.html";
 }
 
 function testroom() {
+	store.forEach(function(key, val) {
+		console.log(key, '==', val);
+	})
 }
