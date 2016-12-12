3e7d7bdcc43c8a114baeb5cb474df55d2aa712ac
zhangxin <zhangxin8105@gmail.com>
2016-12-12 16:40:07 +0800

    增加聊天框架
    为通讯打好框架

---
+++ web/js/chat/chatway.js
@@ -0,0 +1,68 @@
+/*
+ *
+ */
+
+var ChatWay = (function() {
+	var callback = {};
+	var loginName = null;
+	var chatWay = null;
+
+	return {
+		init : initChatWay,
+		login : login,
+		send : send,
+		init : initChatWay,
+	};
+
+	function initChatWay(chat, cb) {
+		chatWay = chat;
+		callback = cb;
+		chatWay.callback.onConnect = function() {
+			if (callback != null && callback.onConnect) {
+				callback.onConnect();
+			}
+		};
+		chatWay.callback.onDisconnect = function() {
+			if (callback != null && callback.onDisconnect) {
+				callback.onDisconnect();
+			}
+		};
+		chatWay.callback.onLoginSuccess = function() {
+			if (callback != null && callback.onLoginSuccess) {
+				callback.onLoginSuccess();
+			}
+		};
+		chatWay.callback.onError = function(err) {
+			if (callback != null && callback.onError) {
+				callback.onError(err);
+			}
+		};
+		chatWay.callback.onSystem = function(nickName, userCount, type) {
+			if (callback != null && callback.onSystem) {
+				callback.onSystem(nickName, userCount, type);
+			}
+		};
+		chatWay.callback.onNewMsg = function(user, msg, color) {
+			if (callback != null && callback.onNewMsg) {
+				callback.onNewMsg(user, msg, color);
+			}
+		};
+		chatWay.callback.onNewImg = function(user, img, color) {
+			if (callback != null && callback.onNewImg) {
+				callback.onNewImg(user, img, color);
+			}
+		};
+
+		chatWay.init();
+	}
+
+	function login(login) {
+		loginName = login;
+		chatWay.login(loginName);
+	}
+
+	function send(msg) {
+		chatWay.sendMsg(msg);
+	}
+
+}());
---
+++ web/js/chat/yunba/yunba.js
@@ -0,0 +1,122 @@
+/*
+ */
+
+var YunbaInf = function() {
+	this.socket = null;
+	this.appkey = null;
+	this.port = null;
+};
+
+YunbaInf.prototype = {
+	callback : {},
+	init : function() {
+		var that = this;
+		this.yunba_demo = new Yunba({
+			appkey : '54d0c24252be1f7e1dd84c42'
+		});
+		this.yunba_demo.init(function(success) {
+			if (success) {
+				that.yunba_demo.connect_by_customid(
+						'yunba_chatroom_demo_77476', function(success, msg) {
+							if (success) {
+								console.log('连接成功!');
+								sub();
+							} else {
+								console.log(msg);
+							}
+						});
+			}
+		});
+
+		function sub() {
+			var e = new Date;
+			that.chatroomTopic = "CHATROOM_DEMO_" + e.getFullYear()
+					+ e.getMonth() + e.getDate();
+			// <!-- 若要接收一个频道的消息，先使用 subscribe() 方法订阅该频道。 -->
+			that.yunba_demo.subscribe({
+				'topic' : that.chatroomTopic
+			}, function(success, msg) {
+				if (success) {
+					console.log('你已成功订阅频道');
+				} else {
+					console.log(msg);
+				}
+			});
+			// <!-- 用 set_message_cb() 设置收到消息时调用的回调函数 -->
+			that.yunba_demo
+					.set_message_cb(function(data) {
+						console.log('data:' + data);
+
+						var msg = data.msg;
+						try {
+							msg = JSON.parse(data.msg);
+						} catch (b) {
+						}
+						console.log('msg:' + msg);
+						if (that.callback != null
+								&& that.callback.onNewMsg != null) {
+							that.callback.onNewMsg(msg.username,
+									msg.dataContent, null);
+						}
+					});
+		}
+
+		var that = this;
+		this.socket = io.connect("sock.yunba.io:443", {
+			force_new_connection : true,
+			secure : true
+		});
+		this.socket.on('connect', function() {
+			if (that.callback != null && that.callback.onConnect != null) {
+				that.callback.onConnect();
+			}
+		});
+		this.socket.on('nickExisted', function() {
+			if (that.callback != null && that.callback.onNickExisted != null) {
+				that.callback.onNickExisted();
+			}
+		});
+		this.socket.on('loginSuccess', function() {
+			if (that.callback != null && that.callback.onLoginSuccess != null) {
+				that.callback.onLoginSuccess();
+			}
+		});
+		this.socket.on('error', function(err) {
+			if (that.callback != null && that.callback.onError != null) {
+				that.callback.onError(err);
+			}
+		});
+		this.socket.on('system', function(nickName, userCount, type) {
+			if (that.callback != null && that.callback.onSystem != null) {
+				that.callback.onSystem(nickName, userCount, type);
+			}
+		});
+		this.socket.on('newImg', function(user, img, color) {
+			if (that.callback != null && that.callback.onNewImg != null) {
+				that.callback.onNewImg(user, msg, color);
+			}
+		});
+		// this.socket.connect();
+	},
+
+	login : function(nickName) {
+		this.socket.emit('login', nickName);
+	},
+
+	sendMsg : function(msg, color) {
+		this.yunba_demo.publish({
+			'topic' : this.chatroomTopic,
+			'msg' : msg
+		}, function(success, msg) {
+			if (success)
+				console.log('消息发布成功');
+			else
+				console.log(msg);
+		});
+	},
+
+	sendImg : function(result, color) {
+		// that.socket.emit('img', e.target.result, color);
+		this.socket.emit('img', result, color);
+	},
+};
