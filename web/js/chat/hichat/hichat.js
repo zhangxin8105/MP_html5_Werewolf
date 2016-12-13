cf245d7bedaee99d25a280b16bdb93965ee7e988
Zhangxin <zhangxin8105@gmail.com>
2016-12-14 00:41:11 +0800

    删除多余代码

--- web/js/chat/hichat/hichat.js
+++ web/js/chat/hichat/hichat.js
@@ -58,43 +58,6 @@ HiChat.prototype = {
 	},
 
 	sendImg : function(result, color) {
-		// that.socket.emit('img', e.target.result, color);
 		this.socket.emit('img', result, color);
 	},
-
-// _initialEmoji : function() {
-// var emojiContainer = document.getElementById('emojiWrapper'), docFragment
-// = document
-// .createDocumentFragment();
-// for (var i = 69; i > 0; i--) {
-// var emojiItem = document.createElement('img');
-// emojiItem.src = '../content/emoji/' + i + '.gif';
-// emojiItem.title = i;
-// docFragment.appendChild(emojiItem);
-// }
-// ;
-// emojiContainer.appendChild(docFragment);
-// },
-// _showEmoji : function(msg) {
-// var match, result = msg, reg = /\[emoji:\d+\]/g, emojiIndex, totalEmojiNum =
-// document
-// .getElementById('emojiWrapper').children.length;
-// while (match = reg.exec(msg)) {
-// emojiIndex = match[0].slice(7, -1);
-// if (emojiIndex > totalEmojiNum) {
-// result = result.replace(match[0], '[X]');
-// } else {
-// result = result.replace(match[0],
-// '<img class="emoji" src="../content/emoji/'
-// + emojiIndex + '.gif" />');// todo:fix this in
-// // chrome it will
-// // cause a new
-// // request for the
-// // image
-// }
-// ;
-// }
-// ;
-// return result;
-// }
 };
