72a3516d00bc445923ef18bd2c1b3c9f6358eea6
Zhangxin <zhangxin8105@gmail.com>
2016-12-14 00:42:06 +0800

    分离初始化通讯模块

--- web/room/js/room.js
+++ web/room/js/room.js
@@ -17,43 +17,45 @@ function testroom() {
 	})
 }
 
+function initChat(){
+    var cb = {};
+    ChatWay.init(new WorkerMan(), cb);
+    cb.onConnect = function() {
+        showMsg("连接服务器成功！\r\n");
+    };
+    cb.onDisconnect = function() {
+        showMsg("断开服务器连接！\r\n");
+    };
+    cb.onLoginSuccess = function() {
+        showMsg("登录成功！\r\n");
+    };
+    cb.onError = function(err) {
+        showMsg("系统错误！" + err + "\r\n");
+    };
+    cb.onSystem = function(nickName, userCount, type) {
+        showMsg(nickName + "登录,用户总数:" + userCount + ",类型:" + type//
+                + "\r\n");
+    };
+    cb.onNewMsg = function(user, msg, color) {
+        showMsg(user + ":" + msg + "(" + color + ")\r\n");
+    };
+    cb.onNewImg = function(user, img, color) {
+        showMsg(user + ":" + img + "(" + color + ")\r\n");
+    };
+
+    var uid = UrlParamHelper.getUrlParamByName(Werewolf.CONST.KEY_USER_ID);
+    if (!uid) {
+        uid = CookieHelper.getCookieByName(Werewolf.CONST.KEY_USER_ID);
+        if (!uid) {
+            uid = "10001";
+        }
+    }
+
+    ChatWay.login(uid);
+}
+
 window.onload = function() {
 	console.log("window.onload");
-
-	var cb = {};
-	ChatWay.init(new YunbaInf5(), cb);
-	cb.onConnect = function() {
-		showMsg("连接服务器成功！\r\n");
-	};
-	cb.onDisconnect = function() {
-		showMsg("断开服务器连接！\r\n");
-	};
-	cb.onLoginSuccess = function() {
-		showMsg("登录成功！\r\n");
-	};
-	cb.onError = function(err) {
-		showMsg("系统错误！" + err + "\r\n");
-	};
-	cb.onSystem = function(nickName, userCount, type) {
-		showMsg(nickName + "登录,用户总数:" + userCount + ",类型:" + type//
-				+ "\r\n");
-	};
-	cb.onNewMsg = function(user, msg, color) {
-		showMsg(user + ":" + msg + "(" + color + ")\r\n");
-	};
-	cb.onNewImg = function(user, img, color) {
-		showMsg(user + ":" + img + "(" + color + ")\r\n");
-	};
-
-	var uid = UrlParamHelper.getUrlParamByName(Werewolf.CONST.KEY_USER_ID);
-	if (!uid) {
-		uid = CookieHelper.getCookieByName(Werewolf.CONST.KEY_USER_ID);
-		if (!uid) {
-			uid = "10001";
-		}
-	}
-
-	ChatWay.login(uid);
 
 	console.log(UrlParamHelper.getUrlParamByName(Werewolf.CONST.KEY_ROOM_NUM));
 	console.log(UrlParamHelper.getUrlParamByName(Werewolf.CONST.KEY_USER_ID));
@@ -66,6 +68,8 @@ window.onload = function() {
 	console.log(CookieHelper.getCookieByName(Werewolf.CONST.KEY_USER_ID));
 	console.log(CookieHelper.getCookieByName(Werewolf.CONST.KEY_USER_NAME));
 	console.log(CookieHelper.getCookieByName(Werewolf.CONST.KEY_PASSWORD));
+
+    initChat();
 };
 
 function showMsg(msg) {
