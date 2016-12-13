5668543810f6f9bc7a8a29ced9ece085cd451354
zhangxin <zhangxin8105@gmail.com>
2016-12-13 17:50:44 +0800

    删除多余的代码

--- web/js/chat/yunba/yunba.js
+++ web/js/chat/yunba/yunba.js
@@ -4,74 +4,65 @@
 var YunbaInf1 = function() {
 	var date = new Date();
 	return new YunbaInf({
-		appKey : "54d0c24252be1f7e1dd84c42",// chat demo
-		customid : "yunba_chatroom_demo_Ww",// "yunba_chatroom_demo_77476",
-		topic : "CHATROOM_DEMO_Ww_" + date.getFullYear() + date.getMonth()
-				+ date.getDate(),
+		appKey : "54d0c24252be1f7e1dd84c42",// chat demo 快速入门 demo
+		customid : "yunba_push_Ww_",// "yunba_chatroom_demo_77476",
+		topic : "Ww_topic",
 	});
 }
 
 var YunbaInf2 = function() {
 	return new YunbaInf({
-		appKey : "54d0c24252be1f7e1dd84c42",// 快速入门 demo
-		customid : "yunba_push_demo_1",
+		appKey : "52fcc04c4dc903d66d6f8f92",// 快速入门
+		customid : "yunba_push_Ww_",
 		topic : "Ww_topic",
 	});
 }
 
 var YunbaInf3 = function() {
 	return new YunbaInf({
-		appKey : "52fcc04c4dc903d66d6f8f92",// 快速入门
-		customid : "yunba_push_Ww",
+		appKey : "56a0a88c4407a3cd028ac2fe",// 视频直播互动 云巴多人绘图 云巴多屏触摸
+		customid : "yunba_push_Ww_",
 		topic : "Ww_topic",
 	});
 }
 
 var YunbaInf4 = function() {
 	return new YunbaInf({
-		appKey : "56a0a88c4407a3cd028ac2fe",// 视频直播互动 云巴多人绘图 云巴多屏触摸
-		customid : "yunba_push_Ww",
+		appKey : "53db1f030416ca3817e33c57",// 【Yo App】聊天软件
+		customid : "yunba_push_Ww_",
 		topic : "Ww_topic",
 	});
 }
 
 var YunbaInf5 = function() {
 	return new YunbaInf({
-		appKey : "53db1f030416ca3817e33c57",// 【Yo App】聊天软件
-		customid : "yunba_push_Ww",
+		appKey : "563c4afef085fc471efdf803",// 云巴智能小屋
+		customid : "yunba_push_Ww_",
 		topic : "Ww_topic",
 	});
 }
 
 var YunbaInf6 = function() {
 	return new YunbaInf({
-		appKey : "563c4afef085fc471efdf803",// 云巴智能小屋
-		customid : "yunba_push_Ww",
+		appKey : "5487f75052be1f7e1dd834e8",// 云巴汽车
+		customid : "yunba_push_Ww_",
 		topic : "Ww_topic",
 	});
 }
 
 var YunbaInf7 = function() {
 	return new YunbaInf({
-		appKey : "5487f75052be1f7e1dd834e8",// 云巴汽车
-		customid : "yunba_push_Ww",
-		topic : "Ww_topic",
-	});
-}
-
-var YunbaInf8 = function() {
-	return new YunbaInf({
 		appKey : "5697113d4407a3cd028abead",// 云巴智能空调
-		customid : "yunba_push_Ww",
+		customid : "yunba_push_Ww_",
 		topic : "Ww_topic",
 	});
 }
-var YunbaInf9 = function() {
+var YunbaInf8 = function() {
 	return new YunbaInf({
 		port : 443,
 		secure : !0,
 		appKey : "54d0c26252be1f7e1dd84c43",// 各个产品页
-		customid : "yunba_push_Ww",
+		customid : "yunba_push_Ww_",
 		topic : "Ww_topic",
 	});
 }
@@ -96,16 +87,19 @@ YunbaInf.prototype = {
 		});
 		this.yunba_demo.init(function(success) {
 			if (success) {
-				that.yunba_demo.connect_by_customid(that.data.customid,
-						function(success, msg) {
-							if (success) {
-								console.log('连接成功!');
-								sub();
-							} else {
-								console.log(msg);
-							}
-						});
+				that.yunba_demo.connect_by_customid(//
+				that.data.customid + Math.floor(1e5 * Math.random()),//
+				function(success, msg) {
+					if (success) {
+						console.log('连接成功!');
+						sub();
+					} else {
+						console.log(msg);
+					}
+				});
 			}
+		}, function(success) {
+			console.log('请重新连接!!!!!!');
 		});
 
 		function sub() {
